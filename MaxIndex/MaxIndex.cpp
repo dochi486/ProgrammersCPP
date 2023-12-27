c01fc24ff5c78a827512ea8366d0af00d268179f
dochi486 <93mskim@gmail.com>
2023-12-27 21:18:48 +0900

    가장 큰 수 찾기

---
+++ MaxIndex/MaxIndex.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array) 
+{
+    vector<int> answer;
+    vector<int> copy = array;
+    
+    sort(array.begin(), array.end());
+
+    int size = array.size();
+    
+    answer.push_back(array[size - 1]);
+    int index = find(copy.begin(), copy.end(), array[size -1]) - copy.begin();
+    answer.push_back(index);
+    
+    return answer;
+}
+
+/*
+* vector<int> solution(vector<int> array) {
+    vector<int> answer;
+    auto t = max_element(array.begin(), array.end());
+    answer.push_back(*t);
+    answer.push_back(distance(array.begin(),t));
+    return answer;
+}
+ */
+
+
+int main(int argc, char* argv[])
+{
+    solution({1, 8, 3});
+    return 0;
+}
