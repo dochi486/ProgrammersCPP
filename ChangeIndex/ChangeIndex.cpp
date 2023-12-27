5e5b3f5764549dfd7ed9b70300af2c17fe8d43cf
dochi486 <93mskim@gmail.com>
2023-12-27 21:23:00 +0900

    인덱스 바꾸기

---
+++ ChangeIndex/ChangeIndex.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int num1, int num2)
+{
+    string answer = "";
+
+    auto temp = ' ';
+    temp = my_string[num1];
+    my_string[num1] = my_string[num2];
+    my_string[num2] = temp;
+    
+    return my_string;
+}
+
+/*
+* string solution(string my_string, int num1, int num2) {
+    swap(my_string[num1],my_string[num2]);
+    return my_string;
+}
+ */
+
+int main(int argc, char* argv[])
+{
+    solution("hello", 1, 2);
+    
+    return 0;
+}
