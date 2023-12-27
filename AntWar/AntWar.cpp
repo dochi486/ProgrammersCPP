eaeda372ea8656c1cc85f561359f4a8dc92dd9a7
dochi486 <93mskim@gmail.com>
2023-12-27 20:06:34 +0900

    개미군단 답안...

--- AntWar/AntWar.cpp
+++ AntWar/AntWar.cpp
@@ -3,67 +3,74 @@ int solution(int hp)
 {
     int answer = 0;
 
-    int admiral = 5;
-    int soldier = 3;
-    int worker = 1;
+    // int admiral = 5;
+    // int soldier = 3;
+    // int worker = 1;
 
-    do
-    {
-        if(hp % admiral != 0)
-        {
-            answer += hp / admiral;
-            hp -= admiral * answer;
+    answer += hp / 5;
+    hp = hp % 5;
+    answer += hp / 3;
+    hp = hp % 3;
+    answer += hp;
 
-            if(hp % soldier != 0)
-            {
-                answer += hp / soldier;
-                hp -= soldier * (hp / soldier);
-
-                if(hp % worker == 0)
-                {
-                    answer += hp / worker;
-                    hp -= worker * (hp / worker);
-                }
-            }
-            else
-            {
-                answer += hp / soldier;
-                hp -= soldier * (hp / soldier);
-
-                if(hp % worker == 0)
-                {
-                    answer += hp / worker;
-                    hp -= worker * (hp / worker);
-                }
-            }
-        }
-        else
-        {
-            if(hp % soldier != 0)
-            {
-                answer += hp / soldier;
-                hp -= soldier * (hp / soldier);
-
-                if(hp % worker == 0)
-                {
-                    answer += hp / worker;
-                    hp -= worker * (hp / worker);
-                }
-            }
-            else
-            {
-                answer += hp / soldier;
-                hp -= soldier * (hp / soldier);
-
-                if(hp % worker == 0)
-                {
-                    answer += hp / worker;
-                    hp -= worker * (hp / worker);
-                }
-            }
-        }
-    }while(hp > 0);
     
+    // do
+    // {
+    //     if(hp % admiral != 0)
+    //     {
+    //         answer += hp / admiral;
+    //         hp -= admiral * answer;
+    //
+    //         if(hp % soldier != 0)
+    //         {
+    //             answer += hp / soldier;
+    //             hp -= soldier * (hp / soldier);
+    //
+    //             if(hp % worker == 0)
+    //             {
+    //                 answer += hp / worker;
+    //                 hp -= worker * (hp / worker);
+    //             }
+    //         }
+    //         else
+    //         {
+    //             answer += hp / soldier;
+    //             hp -= soldier * (hp / soldier);
+    //
+    //             if(hp % worker == 0)
+    //             {
+    //                 answer += hp / worker;
+    //                 hp -= worker * (hp / worker);
+    //             }
+    //         }
+    //     }
+    //     else
+    //     {
+    //         if(hp % soldier != 0)
+    //         {
+    //             answer += hp / soldier;
+    //             hp -= soldier * (hp / soldier);
+    //
+    //             if(hp % worker == 0)
+    //             {
+    //                 answer += hp / worker;
+    //                 hp -= worker * (hp / worker);
+    //             }
+    //         }
+    //         else
+    //         {
+    //             answer += hp / soldier;
+    //             hp -= soldier * (hp / soldier);
+    //
+    //             if(hp % worker == 0)
+    //             {
+    //                 answer += hp / worker;
+    //                 hp -= worker * (hp / worker);
+    //             }
+    //         }
+    //     }
+    // }while(hp > 0);
+    //
     
     return answer;
 }
