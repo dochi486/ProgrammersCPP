96376410c5e4966f88376e4456e3a45a1026bec6
김명수 <mskim@jstair.com>
2023-12-27 17:49:22 +0900

    개미군단 작업 중

---
+++ Ant.cpp
@@ -0,0 +1,25 @@
+﻿#include "Ant.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp)
+{
+    int answer = 0;
+
+    int admiral = 5;
+    int soldier = 3;
+    int worker = 1;
+
+    do
+    {
+        if(hp % admiral != 0)
+        {
+            
+        }
+    }while(hp > 0);
+    
+    
+    return answer;
+}
--- ProgrammersCPP.cpp
+++ ProgrammersCPP.cpp
@@ -2,41 +2,14 @@
 
 #include <string>
 #include <vector>
+#include "Ant.h"
 
 using namespace std;
 
-string solution(string rsp)
-{
-    string answer = "";
-
-    //가위는 2 바위는 0 보는 5로 표현
-    //
-
-    for (auto value : rsp)
-    {
-        switch (value)
-        {
-            case '2':
-                answer += "0";
-                break;
-            case '0':
-                answer += "5";
-                break;
-            case '5':
-                answer += "2";
-                break;
-            default:
-                break;
-            
-        }
-    }
-    
-    return answer;
-}
 
 int main(int argc, char* argv[])
 {
-    solution("205");
+    Ant::solution(999);
     
     return 0;
 }
