9905949b64b243245dae4ab382c7838e013eba09
김명수 <mskim@jstair.com>
2023-12-28 11:10:36 +0900

    문자열 정렬(2) 풀었다.

---
+++ StringSort/StringSort.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string)
+{
+    string answer = "";
+
+    for (int i = 0; i < my_string.size(); ++i)
+    {
+        answer += tolower(my_string[i]);
+    }
+
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
+
+/*
+* string solution(string my_string) {
+    for(auto& ch : my_string)
+    {
+        ch = tolower(ch);
+    }
+    sort(my_string.begin(), my_string.end());
+    return my_string;
+}
+ */
+
+
+int main(int argc, char* argv[])
+{
+    solution("Bcad");
+    return 0;
+}
