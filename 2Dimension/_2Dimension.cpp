5493dc3f22084434f08fe0c13097bd821c28b2a7
김명수 <mskim@jstair.com>
2023-12-28 13:40:44 +0900

    다차원 배열로 바꾸기 문제 검색해서 나온 답안.. + 남의 풀이에서 그나마 이해 잘 되는 답안..

---
+++ 2Dimension/_2Dimension.cpp
@@ -0,0 +1,49 @@
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<int> num_list, int n) 
+{
+    vector<vector<int>> answer;
+    
+    for (int i = 0; i < num_list.size() / n; ++i)
+    {
+        vector<int> v;
+
+        for(int j = 0; j < n; ++j)
+        {
+            v.push_back(num_list[i * n + j]);
+        }
+
+        answer.push_back(v);
+    }
+    
+    return answer;
+}
+
+/*
+*
+vector<vector<int>> solution(vector<int> num_list, int n)
+{
+    vector<vector<int>> answer;
+    for(int i=0; i<num_list.size(); i+=n)
+    {
+        vector<int> temp;
+        for(int j=0; j<n; j++)
+        {
+            temp.push_back(num_list[i+j]);
+        }
+        answer.push_back(temp);
+    }
+    return answer;
+}
+ * 
+ */
+
+int main(int argc, char* argv[])
+{
+    solution({100, 95, 2, 4, 5, 6, 18, 33, 948}, 3);
+    return 0;
+}
