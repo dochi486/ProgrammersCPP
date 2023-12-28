345b359e72d5b70b2a1254712c9371bf8d6e05c7
김명수 <mskim@jstair.com>
2023-12-28 16:10:53 +0900

    7의 개수 한 번에 풀었다

---
+++ Count7/Count7.cpp
@@ -0,0 +1,29 @@
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array)
+{
+    int answer = 0;
+
+    for(int i : array)
+    {
+        auto a = to_string(i);
+
+        for(auto it : a)
+        {
+            if(it == '7')
+                answer++;
+        }
+    }
+    
+    return answer;
+}
+
+int main(int argc, char* argv[])
+{
+    solution({7, 77, 17});
+    return 0;
+}
