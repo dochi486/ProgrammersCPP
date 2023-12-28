ffd61d91deee80d922484c0af74a323ba34f68ab
김명수 <mskim@jstair.com>
2023-12-28 16:06:12 +0900

    한 번만 등장한 문자 -> map을 활용해서 풀기

---
+++ DistinctChar/DistinctChar.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+string solution(string s)
+{
+    string answer = "";
+
+    // map은 오름차순 정렬되므로 정렬할 필요가 없음
+    map<char, int> m;
+
+    for (int i = 0; i < s.size(); ++i)
+    {
+        m[s[i]]++;
+    }
+
+    for(auto i : m)
+    {
+        if(i.second == 1)
+        {
+            answer += i.first;
+        }
+    }
+    
+    return answer;
+}
+
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
