ac295e16061bd14d0f50a5559d93df98eb8fd52a
김명수 <mskim@jstair.com>
2023-12-28 13:49:52 +0900

    팩토리얼 접근 방법만 보고 풀었땅. 예외는 n이 1, 2일 때 처리 해줬음

---
+++ Factorial/Factorial.cpp
@@ -0,0 +1,32 @@
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) 
+{
+    int answer = 0;
+    int sum = 1;
+    
+    for(int i = 1; i <= n; ++i)
+    {
+        sum *= i;
+        
+        if(sum > n)
+        {
+            answer = i - 1;
+            break;
+        }
+
+        answer = i;
+    }
+    
+    return answer;
+}
+
+int main(int argc, char* argv[])
+{
+    solution(2);
+    return 0;
+}
