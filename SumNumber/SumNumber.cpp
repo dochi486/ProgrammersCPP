349a4f7fdd3b1941dcc635b78d12b3b572341928
김명수 <mskim@jstair.com>
2023-12-28 15:15:13 +0900

    숨어있는 수 더하기(2) 검색해서 나온 답안

---
+++ SumNumber/SumNumber.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string)
+{
+    int answer = 0;
+    int n = 0;
+    // vector<int> v;
+
+    for (int i = 0; i < my_string.size(); ++i)
+    {
+        if(isdigit(my_string[i]))
+        {
+            n = n * 10 + my_string[i] - '0';
+            // v.push_back(stoi(my_string.substr(i, 1)));
+        }
+        else
+        {
+            answer += n;
+            n = 0;
+        }
+    }
+
+    // for(int a : v)
+    // {
+    //     answer += a;
+    // }
+    answer += n;
+    
+    return answer;
+}
+
+int main(int argc, char* argv[])
+{
+    solution("aAb1B2cC34oOp");
+    return 0;
+}
