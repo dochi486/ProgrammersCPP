77365a5913bc767379fa73d0989c590eedeef17d
김명수 <mskim@jstair.com>
2023-12-28 11:29:54 +0900

    합성수 찾기 : 검색해서 나온 코드

---
+++ CompositeNumber/CompositeNumber.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    // 가장 작은 합성수는 4니가 4부터 시작
+    // n까지 나눠보면서 약수를 찾고
+    for(int i = 4; i <= n; ++i)
+    {
+        int count = 0;
+        // 약수가 3개 이상이면 합성수
+        for(int j = 1; j <= i; ++j)
+        {
+            if(i % j == 0)
+                count++;
+        }
+
+        if(count >= 3)
+        {
+            answer++;
+        }
+        
+    }
+    
+    return answer;
+}
+
+
+int main(int argc, char* argv[])
+{
+    solution(15);
+    return 0;
+}
