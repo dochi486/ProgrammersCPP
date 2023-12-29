e2f88556cda88cd1edf7c7a20fca86a6fe88acfb
김명수 <mskim@jstair.com>
2023-12-29 15:48:08 +0900

    종이 자르기 너무 쉬움

---
+++ CuttingPaper/CuttingPaper.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int M, int N)
+{
+    int answer = 0;
+
+    answer = M * N - 1;
+    
+    return answer;
+}
+
+int main(int argc, char* argv[])
+{
+    solution(2, 5); // result : 9
+    return 0;
+}
