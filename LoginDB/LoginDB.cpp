47465e9b71f7bbf683f31d1c1449839a105ad1df
Myongsu Kim <93mskim@gmail.com>
2024-01-05 13:34:38 +0900

    로그인 문제 코드 일단 작성

---
+++ LoginDB/LoginDB.cpp
@@ -0,0 +1,32 @@
+//
+// Created by mskim on 2024-01-05.
+//
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db)
+{
+    string answer = "";
+
+    // id, pw 모두 일치하면 login
+    // id만 일치하면 wrong pw
+    // 아예 없는 데이터면 fail
+    for (auto id : db)
+    {
+        if(id_pw[0] == id[0] && id_pw[1] == id[1])
+            answer = "login";
+        else if(id_pw[0] == id[0])
+            answer = "wrong pw";
+        else
+            answer = "fail";
+    }
+
+    return answer;
+}
+int main(int argc, char* argv[])
+{
+    solution({"meosseugi", "1234"}, {{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}});;
+    return 0;
+}
