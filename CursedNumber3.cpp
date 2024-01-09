e83d23cdd8f5c3a1459bb82f97e73e2040d0a245
김명수 <93mskim@gmail.com>
2024-01-09 10:01:33 +0900

    clion으로 만든 파일 커밋

---
+++ CursedNumber3.cpp
@@ -0,0 +1,19 @@
+//
+// Created by mskim on 2024-01-09.
+//
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    return answer;
+}
+
+
+int main()
+{
+
+}
